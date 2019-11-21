2fd4671e24619a58241f1eea4253c016547dc4d4
Allen <allenkis1990@163.com>
2019-11-21 17:06:59 +0800

    梦幻彩虹泡泡

--- projects/jianli/portal/views/home/home.vue
+++ projects/jianli/portal/views/home/home.vue
@@ -189,6 +189,18 @@
                             </div>
                         </a>
                     </div>
+                    <div class="col-md-3 col-xs-6" @click="openDemoPage('caihongpp.html')">
+                        <div class="imgbox">
+                            <img src="@portal/images/05.jpg" alt="作品配图">
+                        </div>
+                        <a href="javascript:void(0)">
+                            <div class="zp-cover">
+                                <h4>梦幻彩虹泡泡</h4>
+                                <p>canvas实现彩虹泡泡动效</p>
+                                <img src="@portal/images/search.png" alt="查看">
+                            </div>
+                        </a>
+                    </div>
                     <!--<div class="col-md-3 col-xs-6 ">
                         <div class="imgbox">
                             <img src="@portal/images/07.jpg" alt="作品配图">
